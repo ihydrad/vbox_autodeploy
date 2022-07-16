326efc546bb050910e00165a2cb40a1f2a97d1e2
ihydrad <s-tehnikum@rambler.ru>
2022-07-16 18:59:23 +0300

    create class

--- __init__.py
+++ __init__.py
@@ -9,87 +9,80 @@ from HSMhelper import Node
 from virtualbox.library import NetworkAttachmentType
 
 target = const.ovf_file
-ip = '172.16.187.11'  
-
 net_conf = {
-        "addr":"192.168.43.11",
-        "mask":"255.255.255.0",
-        "gw":"192.168.43.1"
-        } 
+        "ip_addr": "192.168.43.11",
+        "mask": "255.255.255.0",
+        "gw": "192.168.43.1"
+        }
 
-def percent(progress):
-    print("Complete: ")
-    while progress.percent != 100:
-        sleep(0.1)
-        print(f"{progress.percent}%", end='\r')
-        if progress.canceled:
-            print("Canceled")
+
+class HSMDeploy:
+    __buld_pattern = r'\d+\.\d+\.\d\.\d+'
+    start_hsm_ip = "10.0.2.15"
+
+    def __init__(self, ovf_path, ip) -> None:
+        self._ovf_path = ovf_path
+        self.ip_addr = ip
+        self._machine_name = self.machine_name
+        try:
+            self.hsm_build = re.search(self.__buld_pattern, self.ovf_name)
+            self.hsm_build = self.hsm_build.group(0)
+        except:
+            self.hsm_build = 'hsm'
+
+    @property
+    def machine_name(self):
+        self.ovf_name = os.path.basename(self._ovf_path)
+        return self.ovf_name + self.ip_addr.split('.')[-1]
+
+    @machine_name.setter
+    def machine_name(self, name):
+        self._machine_name = name
+
+    def wait(self, progress):
+        print("Complete: ")
+        while progress.percent != 100:
+            sleep(0.1)
+            print(f"{progress.percent}%", end='\r')
+            if progress.canceled:
+                print("Canceled")
+                return 0
+        return 100
+
+    def run(self):
+        vbox = virtualbox.VirtualBox()
+        appliance = vbox.create_appliance()
+        appliance.read(self._ovf_path)
+        appliance.interpret()
+        conf_machine = appliance.virtual_system_descriptions[0]
+        conf_machine.set_name(self._machine_name)
+        progress = appliance.import_machines()
+        print("========importing machine:")
+        if not self.wait(progress):
             return 0
-    return 100
-
-def get_ver_ovf_machine(full_path):
-    pat = r'\d+\.\d+\.\d\.\d+'
-    name = os.path.basename(full_path)
-    return re.search(pat, name).group(0)
-
-def get_last_ip_octet(ip_addr):
-    return ip_addr.split('.')[-1]
-
-def build_name(target_path, ip_macchine):
-    return get_ver_ovf_machine(target_path) + '_' + \
-        get_last_ip_octet(ip_macchine)
-
-def deploy(target):
-    name = build_name(target, ip)
-    vbox = virtualbox.VirtualBox()
-    ovf = vbox.create_appliance()
-    ovf.read(target)
-    ovf.interpret()  
-    conf_machine = ovf.virtual_system_descriptions[0]
-    conf_machine.set_name(name)
-    #set_adapter_1(conf_machine, "hostOnly")
-    progress = ovf.import_machines()
-    print("========importing machine:")
-    if not percent(progress):
-        return 0 
-    uuid_machines = ovf.machines
-    hsm = vbox.find_machine(uuid_machines[0])
-    # создаем сессию с правами записи
-    tmp = hsm.create_session()
-    # получаем изменяемую копию машины
-    hsm_tmp = tmp.machine
-    eth0 = hsm_tmp.get_network_adapter(0)
-    eth0.host_only_interface = "VirtualBox Host-Only Ethernet Adapter"
-    eth0.attachment_type = NetworkAttachmentType["host_only"]
-    eth0.mac_address = "0800272bf921"
-    # коммитим изменения машины
-    hsm_tmp.save_settings()
-    # освобождаем доступ к машине
-    tmp.unlock_machine()    
-    sleep(5)
-    progress = hsm.launch_vm_process(tmp, "gui", [])
-    print("========starting machine:")
-    if not percent(progress):
-        return 0
-
-def wait_resp(hostname, del_s):
-    cnt = 0
-    while True:
-        if del_s == cnt:
+        uuid_machines = appliance.machines
+        hsm = vbox.find_machine(uuid_machines[0])
+        session = hsm.create_session()
+        hsm_tmp = session.machine
+        eth0 = hsm_tmp.get_network_adapter(0)
+        eth0.host_only_interface = "VirtualBox Host-Only Ethernet Adapter"
+        eth0.attachment_type = NetworkAttachmentType["host_only"]
+        eth0.mac_address = "0800272bf921"
+        hsm_tmp.save_settings()
+        session.unlock_machine() 
+        sleep(5)
+        progress = hsm.launch_vm_process(session, "gui", [])
+        print("========starting machine:")
+        if not self.wait(progress):
             return 0
-        sleep(1)
-        cnt+=1
-        response = os.system("ping -n 1 " + hostname)
-        if response == 0:
-            return 1
+
+    # TODO add method for get names of all network adapers
+
 
 if __name__ == "__main__":
-    def_ip = "10.0.2.15"
-    deploy(target)
-    #wait_resp(def_ip, 100)
-    sleep(100)
-    n15 = Node(def_ip)
+    hsm_deploy = HSMDeploy(target, net_conf["ip_addr"])
+    hsm_deploy.run()
+    n15 = Node(hsm_deploy.start_hsm_ip)
     if n15.config.eth0_set(net_conf):
         print("Complete!")
         n15.config.reboot()
-    
